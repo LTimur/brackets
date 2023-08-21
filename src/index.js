3e1417f3071b572591ecfd9e4cef989e86da34b8
LTimur <ledenev-timur@ya.ru>
2023-08-21 18:11:57 +0500

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsList = bracketsConfig.map(item => {
+        return item.join('');
+    });
+    for (let i = 0; i < bracketsList.length;) {
+        if (str.indexOf(bracketsList[i]) !== -1) {
+          str = str.replace(bracketsList[i], '');
+          i = 0;
+        } else {
+          i++;
+        }
+    };
+    return !str
 }
